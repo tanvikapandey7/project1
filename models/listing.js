1fd27f21e1eabc96e710459050d1d8ba6141da6e
tanvika <tanvikapandey7@gmail.com>
2025-10-31 11:02:31 +0530

    done

---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+ 
+const listingSchema = new Schema ({
+     title: {
+        String ,
+        required: true,
+     },
+     description:String,
+     image:{
+      String,
+     
+     } ,
+     price:Number,
+     location:String,
+     country:String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+modulus.export= Listing ; 
